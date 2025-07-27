14fa4506bdec28977b07094854853d97a140c3c4
David Mikalova <324548+dmikalova@users.noreply.github.com>
2025-07-26 19:40:06 -0700

    feat: get TCO import with rate limiting working

--- amasser/src/bg-tco.js
+++ amasser/src/bg-tco.js
@@ -60,8 +60,6 @@ export const getTcoRefreshToken = async () => {
     return null
   }
 
-  console.log('in refreshtoken:', typeof refreshToken, refreshToken)
-
   return JSON.parse(refreshToken)
 }
 
@@ -73,7 +71,6 @@ export const getTcoUser = async token => {
   const t = {
     token: token,
   }
-  console.log('testing token', t, typeof token)
 
   const body = JSON.stringify(t)
 
@@ -107,24 +104,63 @@ export const getTcoUser = async token => {
   }
 
   const respJson = await response.json()
-  console.log('Fetched TCO user info:', respJson)
   if (!respJson.success) {
     await chrome.storage.local.remove('tcoRefreshToken')
     throw new Error(`Failed to fetch user: ${respJson.error}`)
   }
 
-  return { username: respJson.user.username, token: respJson.token }
+  console.log('TCO user info fetched:', respJson)
+
+  return {
+    username: respJson.user.username,
+    token: respJson.token,
+    userId: respJson.user.id,
+  }
 }
 
 const importDecksToTco = async decks => {
   const { token } = await getTcoUser(await getTcoRefreshToken())
 
   // Filter out decks that already have tco=true
-  const decksToImport = Object.values(decks).filter(
-    deck => deck.mv && !deck.tco,
-  )
+  let decksToImport = Object.values(decks).filter(deck => deck.mv && !deck.tco)
 
-  // TODO: get deck list from TCO and import at a slow rate
+  if (decksToImport.length === 0) {
+    console.log('No new decks to import')
+    return decks
+  }
+
+  const { decks: tcoDecks } = await fetch(
+    `${TCO_BASE_URL}/api/decks?pageSize=100000&page=1`,
+    {
+      credentials: 'include',
+      headers: {
+        accept: '*/*',
+        'accept-language': 'en-US',
+        authorization: `Bearer ${token}`,
+        'cache-control': 'no-cache',
+        'content-type': 'application/json',
+        pragma: 'no-cache',
+        'x-requested-with': 'XMLHttpRequest',
+      },
+      method: 'GET',
+    },
+  )
+    .then(response => response.json())
+    .catch(error => {
+      throw new Error(`Failed to fetch TCO decks: ${tcoDecks.error}`)
+    })
+
+  console.log('Fetched TCO decks:', tcoDecks.length)
+  tcoDecks.forEach(tcoDeck => {
+    if (decks[tcoDeck.uuid]) {
+      console.log(`Deck ${tcoDeck.uuid} already exists in local decks`)
+      decks[tcoDeck.uuid].tco = true
+    }
+  })
+
+  decksToImport = Object.values(decks).filter(deck => deck.mv && !deck.tco)
+
+  console.log('Decks to import to TCO: ', decksToImport.length)
 
   for (const [i, deck] of decksToImport.entries()) {
     console.log(
@@ -137,7 +173,7 @@ const importDecksToTco = async decks => {
       headers: {
         accept: '*/*',
         'accept-language': 'en-US',
-        authorization: 'Bearer ' + token,
+        authorization: `Bearer ${token}`,
         'cache-control': 'no-cache',
         'content-type': 'application/json',
         pragma: 'no-cache',
@@ -153,6 +189,11 @@ const importDecksToTco = async decks => {
 
     const respJson = await response.json()
 
+    const tcoImportErrorMessages = [
+      "Cannot read properties of undefined (reading 'house')",
+      'This deck is from a future expansion and not currently supported',
+    ]
+
     if (response.ok && respJson.success) {
       console.log(`Imported ${deck.id}`)
       decks[deck.id].tco = true
@@ -163,9 +204,18 @@ const importDecksToTco = async decks => {
     ) {
       console.log(`Deck already imported to TCO ${deck.id}`)
       decks[deck.id].tco = true
+    } else if (
+      response.ok &&
+      !respJson.success &&
+      tcoImportErrorMessages.some(msg => respJson.message.includes(msg))
+    ) {
+      console.log(
+        `Failed to import to TCO with known error ${deck.id}`,
+      )
+      decks[deck.id].tco = 'import error'
     } else {
       console.error(
-        `Failed to import to TCO ${deck.id}: ${
+        `Failed to import to TCO with unknown error ${deck.id}: ${
           response.status
         } ${JSON.stringify(respJson)}`,
       )
@@ -178,6 +228,9 @@ const importDecksToTco = async decks => {
         button: SYNC_MSGS[i % SYNC_MSGS.length],
       })
       .catch(() => {})
+
+    console.log(`Waiting 5 seconds before next import due to rate limits...`)
+    await new Promise(resolve => setTimeout(resolve, 5000))
   }
   return decks
 }
