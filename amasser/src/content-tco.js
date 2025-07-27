0ed118109b34733722081522428b7b877c869aec
David Mikalova <324548+dmikalova@users.noreply.github.com>
2025-07-26 22:39:31 -0700

    fix: loading tco token

--- amasser/src/content-tco.js
+++ amasser/src/content-tco.js
@@ -1,11 +1,10 @@
-chrome.storage.local.set(
-  { tcoRefreshToken: window.localStorage.getItem('refreshToken') },
-  () => {
-    console.log('KF Amasser: Saved TCO refresh token to extension local storage')
-  },
-)
-
-
-
-// TODO: button for prophecy assignments
-
+if (window.location.href === 'https://thecrucible.online/') {
+  chrome.storage.local.set(
+    { tcoRefreshToken: window.localStorage.getItem('refreshToken') },
+    () => {
+      console.log(
+        'KF Amasser: Saved TCO refresh token to extension local storage',
+      )
+    },
+  )
+}
